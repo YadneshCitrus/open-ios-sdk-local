f0b626a5fd9a3026467ab136b1adc7f51c182690
Mukesh Patil <mukeshpatil@hEms-oSX.local>
2014-09-04 18:33:36 +0530

    Initial Commit

---
+++ SDKSandbox/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  SDKSandbox
+//
+//  Created by Mukesh Patil on 04/09/14.
+//  Copyright (c) 2014 CitrusPay. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
