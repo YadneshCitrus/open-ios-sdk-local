d98d2418ad2b33a0cb2d9e1a3c6be4b1a4ca1552
YadneshCitrus <yadnesh.wankhede@citruspay.com>
2014-10-16 17:12:06 +0530

    testemail

--- Controllers/TestParams.h
+++ Controllers/TestParams.h
@@ -10,7 +10,7 @@
 #define RestFulltester_TestParams_h
 // TestParams.h should be populated according to your needs
 
-#define TEST_EMAIL @"yaddyboy@gmail.com"
+#define TEST_EMAIL @"yaddyboy007@gmail.com"
 #define TEST_PASSWORD @"tester@123"
 #define TEST_MOBILE @"1234567890"
 
